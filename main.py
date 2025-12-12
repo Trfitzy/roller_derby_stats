7293bb11267bcd26aaff6d368a2bf945df3b265a
Trfitzy <96790979+Trfitzy@users.noreply.github.com>
2025-12-12 15:51:18 -0500

    model set up
    Something is still broken with the model. Not sure what yet.

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from sklearn.linear_model import Ridge
 from sklearn.model_selection import cross_val_score, KFold
 
 import warnings
+from model import Model
 
 warnings.filterwarnings('ignore')
 
@@ -27,64 +28,10 @@ def parse_args():
 if __name__ == "__main__":
     input_args = parse_args()
 
-    # WIP v
-    X_train, y_train = load_data(input_args.data_path + "/Data/train")
-    X_test, y_test = load_data(input_args.data_path + "/Data/test")
+    model = Model('VL')
+    model.preprocess_data()
+    model.train_test_known_data()
 
-    data = {
-    'X_train': X_train,
-    'y_train': y_train,
-    'X_test': X_test,
-    'y_test': y_test
-    }
-
-    filter_num = 16
-    model_info = {
-            'epochs': 50,
-            'learning_rate': 0.01,
-            'loss':  "binary_crossentropy", #'MeanSquaredError', #keras.losses.SparseCategoricalCrossentropy(), # multiclass crossentropy
-            "activation": 'relu',
-            "final_activation": 'softmax',
-            'optimizer':'adam',
-            'name': 'u_net',
-            'metrics':["accuracy",keras.metrics.BinaryIoU(target_class_ids=(0, 1), threshold=0.5, name=None, dtype=None),]
-        }
-
-    for num_layers in [2, 3, 4, 5]:
-        model_info['name'] = str(num_layers)+" layers u_net"
-
-        # Build Model
-        model = build_u_model(num_layers, filter_num)
-        model.summary()
-
-        weights_filepath = input_args.data_path + "/DL_HW3_Fitzgerald/weights/U-Net_" + str(num_layers) + "_layers.weights.h5"
-
-        # Check train or test
-        if input_args.action == "train":
-            
-            # Train Model
-            hist = fit_n_assess_model(model, model_info, data, input_args.data_path + "/DL_HW3_Fitzgerald/plots/")
-
-            # Save Model weights
-            model.save_weights(weights_filepath, overwrite=True)
-            print("Weights saved to " + weights_filepath)
-
-        if input_args.action == "test":
-
-            # Load saved weights
-
-            print("Loading model weights from "+weights_filepath)
-            #saved_model = keras.models.load_model(weights_filepath)
-            model.load_weights(weights_filepath)
-
-            # Run prediction
-            y_pred = model.predict(X_test, batch_size=None, steps=None, callbacks=None)    
-
-            # Plot results for image 10
-            plot_images(X_test[10],y_test[10],[y_pred[10]],input_args.data_path + "/DL_HW3_Fitzgerald/plots/",[str(num_layers) + "_layers"])
-
-        else:
-            print("No action entered")
     
     #except FileNotFoundError:
     #    print("The file path " + input_args.data_path + " does not contain train & test folders or those folders are missing files.")
--- model.py
+++ model.py
@@ -2,18 +2,23 @@
 import numpy as np
 import pandas as pd
 
+from sklearn.linear_model import Ridge
+from sklearn.model_selection import cross_val_score, KFold
+import matplotlib.pyplot as plt
+
 from preprocess_functions import pull_data, get_X_features_crg, get_X_features_op, get_X_skater_labels
 
 class Model:
 
-    def __init__(self, crg_team):
+    def __init__(self, crg_team, alpha=20):
         # features need to be hardcoded because they have unique preprocessing.
         self.crg_team = crg_team #options: VL or BS
         self.X_features = [
-            'jam_id', 'lead', 'no_initial', 'trips', 
+            'jam_id', 'lead', #'no_initial', 
+            'trips', 
             'jammer_penalty_counter', 'blocker_penalty_counter',
-            'op_lead', 'op_no_initial', 'op_trips', 
-            'op_jammer_penalty_counter', 'op_blocker_penalty_counter'
+            #'op_lead', 'op_no_initial', 'op_trips', 
+            #'op_jammer_penalty_counter', 'op_blocker_penalty_counter'
             ]
         
         self.X_skater_labels = []
@@ -22,8 +27,9 @@ class Model:
         self.df_data = pull_data(self.crg_team)
         self.df_features = pd.DataFrame()
 
+        self.alpha=20
+
     def preprocess_data(self):
-        print('hello')
         
         # TODO: Error checking
         self.df_features['point_diff'] = self.df_data['Jam Total'] - self.df_data['OP_Jam Total']
@@ -34,7 +40,42 @@ class Model:
     
         self.X_columns = self.X_features + self.X_skater_labels
 
-    #def train(self):
+    def train_test_known_data(self):
+        ridge_model = Ridge(alpha=self.alpha)
+        
+        train_set = self.df_features.sample(frac=0.8, random_state=42)
+    
+        X_train = train_set[self.X_columns]
+        y_train = train_set['point_diff']
+
+        test_set = self.df_features.drop(train_set.index)
+
+        # TODO: what can be toggled for a better outcome
+        cv = KFold(n_splits=5, shuffle=True, random_state=42) 
+        neg_mse_scores = cross_val_score( ridge_model, X_train, y_train, cv=cv, scoring="neg_mean_squared_error" )
+        rmse_scores = np.sqrt(-neg_mse_scores) 
+        r2_scores = cross_val_score(ridge_model, X_train, y_train, cv=cv, scoring="r2") 
+        
+        print("CV RMSE:", rmse_scores.mean(), "+/-", rmse_scores.std()) 
+        print("CV R^2:", r2_scores.mean(), "+/-", r2_scores.std()) 
+
+        #Fit the final model on all data:
+        ridge_model.fit(X_train, y_train)
+
+        y_test = ridge_model.predict(test_set[self.X_columns])
+
+        plt.fill_between(range(0,len(y_test)), test_set['point_diff']-3, test_set['point_diff']+3)
+        plt.plot(test_set['point_diff'],'--o', label='y_pred', color='black')
+        plt.plot(y_test,'-o',label='y_actual',color='orange')
+
+        plt.title ("y_pred compared to y_actual\n average error: "+ str(round(np.mean(abs(test_set['point_diff'] - y_test)),4)))
+        plt.legend()
+        plt.grid()
+
+    def train_test_avg_data(self):
+        alpha = 20
+
+    
 
     #def test(self):
 
@@ -42,3 +83,4 @@ class Model:
 
 model = Model('VL')
 model.preprocess_data()
+model.train_test_known_data()
